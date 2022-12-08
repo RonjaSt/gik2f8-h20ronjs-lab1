e67fe85677bf037cfe72b950759e8b9249e36840
Lord_tensoon <89915147+RonjaSt@users.noreply.github.com>
2022-12-08 10:11:52 +0100

    tog bort kommentarer

--- gik2f8-master/gik2f8-master/Components/BookListItem.js
+++ gik2f8-master/gik2f8-master/Components/BookListItem.js
@@ -1,12 +1,4 @@
-/* const BookListItem = (book) => {
-    
 
-    let html = `<li 
-    class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-amber-500 last:border-b-0 border-b border-zinc-700 cursor-pointer">
-    ${book.author} -${book.title}
-    </li>`; 
-    return html;
-} */
 const BookListItem= (book) => {
     
 
